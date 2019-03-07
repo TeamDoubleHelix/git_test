16a920c5e4e9665aa20c9c5926260cd2fed3f651
Megan Montebello <MeganM@Megans-MacBook-Air-2.local>
2019-03-07 18:46:34 +0100

    created b

--- lib/animalScreen.dart
+++ lib/animalScreen.dart
@@ -7,6 +7,9 @@ class AnimalScreen extends StatefulWidget {
 }
 
 class AnimalScreenState extends State<AnimalScreen> {
+
+  var b;
+
   @override
   Widget build(BuildContext context) {
     return Container(
