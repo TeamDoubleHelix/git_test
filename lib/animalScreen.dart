2845047febb25671489deba6eaf0218a75fb8824
Megan Montebello <MeganM@Megans-MacBook-Air-2.local>
2019-03-07 18:42:56 +0100

    added animal screen

---
+++ lib/animalScreen.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/material.dart';
+
+class AnimalScreen extends StatefulWidget {
+  @override
+  AnimalScreenState createState() => AnimalScreenState();
+}
+
+class AnimalScreenState extends State<AnimalScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
